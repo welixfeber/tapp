3d1e1d6969e89c74e654dc6e179a21ca73cdf830
Felix Henninger <mailbox@felixhenninger.com>
2024-06-17 22:06:13 +0200

    Add and apply Assistant font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        sans: ['"Krub"'],
+        sans: ['"Assistant Variable"'],
       },
       colors: {
         accent: '#071A2D',
